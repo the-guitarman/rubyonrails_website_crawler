4ec80002fba4838ed21764ed33128d0b85731434
guitarman <guitarman78@freenet.de>
2021-01-04 17:13:11 +0100

    assets changed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,6 +5,8 @@ environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
+    jquery: 'jquery',
+    window.jQuery: 'jquery',
     Popper: ['popper.js', 'default']
   })
 )
